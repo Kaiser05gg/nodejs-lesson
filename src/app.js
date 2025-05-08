0187c12aef39e2cdf9cd307ecd8bbb25dfbb2c0c
Kaiser05gg <shu.iss1121@icloud.com>
2025-05-08 21:37:29 +0900

    3  commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const server = app.listen(3000, function () {
+  console.log("node.js is listening to PORT:" + server.address().port);
+})
+
+app.disable("x-powered-by");
+app.use(cors()).use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.json({
+    id: "1",
+    name: "hogehoge",
+    description: "hello world",
+  });
+});
+
 console.log("hello!");
